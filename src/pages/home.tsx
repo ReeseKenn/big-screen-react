b20638fab189c67c6619e0b8a0e4b8bee887c7f6
ReeseKenn <yanbaorui1997@163.com>
2023-02-02 11:53:26 +0800

    add header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './home.scss';
+import headerBg from '../images/headerBg.png';
 
 export const Home = () => {
-  return (
-    <div>
-      hello
-    </div>
-  );
+    return (
+        <div className="home">
+            <header style={{backgroundImage: `url(${headerBg})`}}>启程跨境运营战略平台</header>
+        </div>
+    );
 };
