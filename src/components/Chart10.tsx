33395f7652a0c955c753e705d2570665f84b38c4
ReeseKenn <yanbaorui1997@163.com>
2023-02-20 11:29:03 +0800

    add countUp

--- src/components/Chart10.tsx
+++ src/components/Chart10.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CountUp from "react-countup";
 
 export const Chart10 = () => {
     return (
@@ -6,42 +7,82 @@ export const Chart10 = () => {
             <div className="card other-bordered">
                 <div className="cardTitle">成交额</div>
                 <div className="cardNumber">
-                    <div className="number">$4394.88</div>
+                    <CountUp
+                        className="number"
+                        start={0}
+                        end={4394.88}
+                        duration={10}
+                        decimals={2}
+                        prefix="$"
+                    />
                     <div className="per decrease">12%</div>
                 </div>
             </div>
             <div className="card other-bordered">
                 <div className="cardTitle">订单量</div>
                 <div className="cardNumber">
-                    <div className="number">464</div>
+                    <CountUp
+                        className="number"
+                        start={0}
+                        end={464}
+                        duration={10}
+                        decimals={0}
+                    />
                     <div className="per increase">5%</div>
                 </div>
             </div>
             <div className="card other-bordered">
                 <div className="cardTitle">客单价</div>
                 <div className="cardNumber">
-                    <div className="number">$60.94</div>
+                    <CountUp
+                        className="number"
+                        start={0}
+                        end={60.94}
+                        duration={10}
+                        decimals={2}
+                        prefix="$"
+                    />
                     <div className="per decrease">7%</div>
                 </div>
             </div>
             <div className="card other-bordered">
                 <div className="cardTitle">预估毛利率</div>
                 <div className="cardNumber">
-                    <div className="number">53.15%</div>
+                    <CountUp
+                        className="number"
+                        start={0}
+                        end={53.15}
+                        duration={10}
+                        decimals={2}
+                        suffix="%"
+                    />
                     <div className="per decrease">3%</div>
                 </div>
             </div>
             <div className="card other-bordered">
                 <div className="cardTitle">营销花费</div>
                 <div className="cardNumber">
-                    <div className="number">￥7592.69</div>
+                    <CountUp
+                        className="number"
+                        start={0}
+                        end={7592.69}
+                        duration={10}
+                        decimals={2}
+                        prefix="$"
+                    />
                     <div className="per decrease">11%</div>
                 </div>
             </div>
             <div className="card other-bordered">
                 <div className="cardTitle">投入产出比</div>
                 <div className="cardNumber">
-                    <div className="number">3.72</div>
+                    <CountUp
+                        className="number"
+                        start={0}
+                        end={3.72}
+                        duration={10}
+                        decimals={2}
+                    />
                     <div className="per increase">6.5%</div>
                 </div>
             </div>
